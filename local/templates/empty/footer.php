3f2f004f98020ab62a4bca8474a2ed3ff16ef351
mrvinil <mrvinil2061@gmail.com>
2022-07-14 21:57:50 +0300

    feature: footer done

--- local/templates/empty/footer.php
+++ local/templates/empty/footer.php
@@ -4,7 +4,18 @@
 
 </main>
 
-<footer class="footer"></footer>
+<footer class="footer gap">
+	<div class="section footer__info">
+		<div class="footer__logo-wrap">
+			<a href="/" class="footer__logo link"></a>
+			<p class="footer__address">Режим работы: Пн–Пт с 9:00 до 19:00</p>
+		</div>
+		<div class="footer__contacts">
+			<a href="tel:88124256215" class="phone-link link">(812) 425-62-15</a>
+			<a href="#" class="callback-link link">заказать звонок</a>
+		</div>
+	</div>
+</footer>
 
 
 <?//php require_once $_SERVER['DOCUMENT_ROOT'] . SITE_TEMPLATE_PATH . '/assets/templates/callback.php';?>
