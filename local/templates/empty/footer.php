498ef630c4b84726c998be5b56ef43168ffb11ee
Vyacheslav K <i@kvs142.ru>
2022-08-11 13:15:49 +0300

    feature: добавил виджет чата

--- local/templates/empty/footer.php
+++ local/templates/empty/footer.php
@@ -30,5 +30,12 @@
 </footer>
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . SITE_TEMPLATE_PATH . '/assets/templates/callback.php';?>
 
+<script>
+	(function(w,d,u){
+		var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+		var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+	})(window,document,'https://cdn-ru.bitrix24.ru/b5901341/crm/site_button/loader_1_rp61zn.js');
+</script>
+
 </body>
 </html>
